0ed54e396c6d4a3d5327b487af14b9d96a707482
lagzikovacs <lks@docport.hu>
2017-10-21 07:20:14 +0200

    első

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule, Headers } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooldalComponent } from './fooldal/fooldal.component';
+import { AjanlatkeresComponent } from './ajanlatkeres/ajanlatkeres.component';
+import { HibabejelentesComponent } from './hibabejelentes/hibabejelentes.component';
+import { RolunkComponent } from './rolunk/rolunk.component';
+import { ReferenciakComponent } from './referenciak/referenciak.component';
+import { FogalomtarComponent } from './fogalomtar/fogalomtar.component';
+import { GyikComponent } from './gyik/gyik.component';
+import { TervezesComponent } from './tervezes/tervezes.component';
+import { EszkozokComponent } from './eszkozok/eszkozok.component';
+import { PartnerekComponent } from './partnerek/partnerek.component';
+import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
+
+export const routerSettings: Routes = [
+  { path: 'fooldal', component: FooldalComponent },
+  { path: 'ajanlatkeres', component: AjanlatkeresComponent },
+  { path: 'referenciak', component: ReferenciakComponent },
+  { path: 'kapcsolat', component: KapcsolatComponent },
+  { path: '', component: FooldalComponent },
+  { path: '**', component: FooldalComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooldalComponent,
+    AjanlatkeresComponent,
+    HibabejelentesComponent,
+    RolunkComponent,
+    ReferenciakComponent,
+    FogalomtarComponent,
+    GyikComponent,
+    TervezesComponent,
+    EszkozokComponent,
+    PartnerekComponent,
+    KapcsolatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routerSettings),
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
