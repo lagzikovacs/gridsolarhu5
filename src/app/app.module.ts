4f9e0495450cc0d23ed0efa3f1e29e2033498e91
lagzikovacs <lks@docport.hu>
2017-10-21 07:33:20 +0200

    végső

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpModule, Headers } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooldalComponent } from './fooldal/fooldal.component';
 import { AjanlatkeresComponent } from './ajanlatkeres/ajanlatkeres.component';
-import { HibabejelentesComponent } from './hibabejelentes/hibabejelentes.component';
-import { RolunkComponent } from './rolunk/rolunk.component';
 import { ReferenciakComponent } from './referenciak/referenciak.component';
-import { FogalomtarComponent } from './fogalomtar/fogalomtar.component';
-import { GyikComponent } from './gyik/gyik.component';
-import { EszkozokComponent } from './eszkozok/eszkozok.component';
-import { PartnerekComponent } from './partnerek/partnerek.component';
 import { KapcsolatComponent } from './kapcsolat/kapcsolat.component';
 
 export const routerSettings: Routes = [
@@ -32,13 +26,7 @@ export const routerSettings: Routes = [
     MenuComponent,
     FooldalComponent,
     AjanlatkeresComponent,
-    HibabejelentesComponent,
-    RolunkComponent,
     ReferenciakComponent,
-    FogalomtarComponent,
-    GyikComponent,
-    EszkozokComponent,
-    PartnerekComponent,
     KapcsolatComponent
   ],
   imports: [
